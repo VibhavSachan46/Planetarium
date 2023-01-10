6f8f6f00a7e9cfecc45a278d996810abc0e82318
Vibhav <vibhavsachan743@gmail.com>
2023-01-10 23:35:07 +0530

    UI completed

--- app/src/main/java/com/example/planetarium/Adapter.kt
+++ app/src/main/java/com/example/planetarium/Adapter.kt
@@ -1,5 +1,6 @@
 package com.example.planetarium
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -27,10 +28,53 @@ class Adapter(var planet:List<PlanetData>) : RecyclerView.Adapter<Adapter.MyView
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         // its role is to to set data inside view
 
+        var dummyImage: Int?=null
+        holder.itemView.setOnClickListener{
+            val intent = Intent(holder.itemView.context, PlanetDetail::class.java)
+            intent.putExtra("planet" ,planet[position])
+            intent.putExtra("planetImage",dummyImage)
+            holder.itemView.context.startActivity(intent)
+        }
         holder.title.text = planet[position].title
         holder.galaxy.text = planet[position].galaxy
         holder.distance.text = planet[position].distance+ "m km"
         holder.gravity.text = planet[position].gravity+ "m/ss"
+
+        when(planet[position].title!!.lowercase())
+        {
+            "mars"->{
+                dummyImage = R.drawable.ic_mars
+            }
+            "neptune"->{
+                dummyImage = R.drawable.ic_neptune
+            }
+            "earth"->{
+                dummyImage = R.drawable.ic_earth
+            }
+            "moon"->{
+                dummyImage = R.drawable.ic_moon
+            }
+            "venus"->{
+                dummyImage = R.drawable.ic_venus
+            }
+            "jupiter"->{
+                dummyImage = R.drawable.ic_jupiter
+            }
+            "saturn"->{
+                dummyImage = R.drawable.ic_saturn
+            }
+            "uranus"->{
+                dummyImage = R.drawable.ic_uranus
+            }
+            "mercury"->{
+                dummyImage = R.drawable.ic_mercury
+            }
+            "sun"->{
+                dummyImage = R.drawable.ic_sun
+            }
+        }
+
+        holder.planetimg.setImageResource(dummyImage!!)
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/planetarium/PlanetData.kt
+++ app/src/main/java/com/example/planetarium/PlanetData.kt
@@ -1,5 +1,8 @@
 package com.example.planetarium
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class PlanetData(
     val id:Int,
     val title:String?,
@@ -7,4 +10,38 @@ data class PlanetData(
     val distance:String?,
     val gravity:String?,
     val overview:String?
-)
+):Parcelable{
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(title)
+        parcel.writeString(galaxy)
+        parcel.writeString(distance)
+        parcel.writeString(gravity)
+        parcel.writeString(overview)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<PlanetData> {
+        override fun createFromParcel(parcel: Parcel): PlanetData {
+            return PlanetData(parcel)
+        }
+
+        override fun newArray(size: Int): Array<PlanetData?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
